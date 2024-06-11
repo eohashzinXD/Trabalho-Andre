7c12b4c75be3bc6005c0c37f2d0b7cbd6b332a06
Paulo Junior <104381009+eohashzinXD@users.noreply.github.com>
2024-06-11 18:06:22 -0300

    Tabalho quase feito

--- main.c
+++ main.c
@@ -1,91 +1,265 @@
-#include <stdio.h>
 #include "structs.h"
-#include <string.h>
 
-void menu(){
-    Passageiro pessoas;
-    int op = -1;
-
-    printf("Digite uma opcao: \n");
-    printf("1 - Consultar Passagens Disponiveis\n");
-    printf("2 - Fazer Reserva\n");
-    printf("3 - Listar Reservas\n");
-    printf("4 - Cancelar Reserva\n");
-    printf("5 - Sair\n");
-    scanf("%d", &op);
-
-    do
-    {
-        switch (op)
-        {
-        case 1:
-            //vooDispo();
-            break;
-        case 2:
-            fazerReserva(&pessoas, "passageiros.txt");
-            break;
-        case 3:
-            //listarReservas();
-            break;
-        case 4:
-            //cancelarReserva();
-            break;
-        case 5:
-            break;
-        
-        default:
-            printf("Opcao invalida");
-            break;
-        }
-    } while (op != 5);
-}
-
-int fazerReserva(Passageiro *pessoas, const char* filename){
-    Passageiro unico;
-    char *caractere_nao_convertido;
-
-    char linha[256];
+int carregarPassageiros(Passageiros *pessoas, const char *filename){
+    Passageiros unico;
+    char linha[118];
     char nome[200];
-    char sobrenome[200];
-    char sobrenome_sobre[200];
-    char ultimo_nome[200];
-    char doc[20];
+    char nDoc[7];
+    char *caractere_nao_coonvertido;
 
-    FILE *txtFile = fopen(filename, "r");
-    if (!txtFile)
+    FILE *file = fopen(filename, "r");
+    if (!file)
     {
+        printf("Erro ao abrir o arquivo!");
         return 1;
     }
 
     int count = 0;
-    while (fgets(linha, sizeof(linha), txtFile))
+    while (fgets(linha, sizeof(linha), file))
     {
-        sscanf(linha, "%s %s %s %s %s\n", nome, sobrenome, sobrenome_sobre, ultimo_nome, doc);
+        sscanf(linha, "%s %s\n", nome, nDoc);
+        
 
-        unico.numero_doc = strtol(doc, &caractere_nao_convertido, 10);
+        unico.numDoc = strtol(nDoc, &caractere_nao_coonvertido, 10);
         snprintf(unico.nome, sizeof(unico.nome), "%s", nome);
-        snprintf(unico.segundo, sizeof(unico.segundo), "%s", sobrenome);
-        snprintf(unico.sobrenome, sizeof(unico.sobrenome), "%s", sobrenome_sobre);
-        snprintf(unico.ultimo, sizeof(unico.ultimo), "%s", ultimo_nome);
-        printf("%s\n", doc);
 
         pessoas[count] = unico;
-        //printf("Reg Nome: %s %s %s %s\nNumero Doc: %ld\n", pessoas[count].nome,pessoas[count].segundo,pessoas[count].sobrenome,pessoas[count].ultimo, pessoas[count].numero_doc);
+        //printf("%s %s\n", nome, nDoc); //para debug
+        //printf("Reg: %s %d\n", pessoas[count].nome, pessoas[count].numDoc);//debug reg
         count++;
     }
-    fclose(txtFile);
     
+    fclose(file);
     return count;
+}
+
+int carregarVoos(Voos *voo, const char *filename){
+    Voos unico;
+    int count = 0;
+    char *caractere_nao_convertido;
+    char linha[556], origem[15], destino[15], string_data[11], string_hora[6], assentos[4];
+    char dia[3], mes[3], ano[5], hora[3], min[4];
+    FILE *file = fopen(filename, "r");
+    if (!file)
+    {
+        printf("Erro ao abrir o arquivo de voo");
+        return -2;
+    }
+
+    while (fgets(linha, sizeof(linha), file))
+    {
+        sscanf(linha, "%s %s %s %s %s", origem, destino, string_data, string_hora, assentos);
+        //printf("Linha: %s %s %s %s %s\n", origem, destino, string_data, string_hora, assentos); 
+
+        sscanf(string_data, "%2[0-9]/%2[0-9]/%4[0-9]", dia, mes, ano);
+        sscanf(string_hora, "%2[0-9]:%2[0-9]", hora, min);
+        //printf("Data: %2s/%2s/%4s\n", dia, mes, ano);
+        unico.assentosDispo = strtol(assentos, &caractere_nao_convertido, 10);
+        unico.dia = strtol(dia, &caractere_nao_convertido, 10);
+        unico.mes = strtol(mes, &caractere_nao_convertido, 10);
+        unico.ano = strtol(ano, &caractere_nao_convertido, 10);
+        unico.hora = strtol(hora, &caractere_nao_convertido, 10);
+        unico.min = strtol(min, &caractere_nao_convertido, 10);
+        snprintf(unico.origem, sizeof(unico.origem), "%s", origem);
+        snprintf(unico.destino, sizeof(unico.destino), "%s", destino);
+
+        //printf("Reg %d/%d/%d %s %s %d:%d %d\n", unico.dia, unico.mes, unico.ano, unico.origem, unico.destino, unico.hora, unico.min, unico.assentosDispo);
+
+        voo[count] = unico;
+        count++;
+    }
+    fclose(file);
+    return count;
+
+    
+}
+
+void fazerReservas(Passageiros *pessoas,int numPassageiros, Voos *voo, int numVoos, Reserva *reservas, int *numReservas){
+    int idPass = 0, idvoo = 0;
+    //printf("Id Passageiro: %d\n", numPassageiros );//debug
+    fflush(stdin);
+    printf("Digite o ID do passageiro (0 a %d):", numPassageiros - 1);
+    idPass = input_d("");
+    //printf("Id Passageiro: %d\n", idPass);//debug
+    if (idPass < 0 || idPass >= numPassageiros)
+    {
+        printf("ID do Passageiro Invalido!");
+        return;
+    }
+    fflush(stdin);
+    printf("Digite o ID do voo (0 a %d):", numVoos - 1);
+    idvoo = input_d("");
+    //printf("Id Voo: %d\n", idvoo);//debug
+    if (idvoo < 0 || idvoo >= numVoos)
+    {
+        printf("ID do Voo Invalido!");
+        return;
+    }
+    if (voo[idvoo].assentosDispo > 0) {
+        voo[idvoo].assentosDispo--;
+        reservas[*numReservas].idPassageiro = idPass;
+        reservas[*numReservas].idVoo = idvoo;
+        (*numReservas)++;
+
+        printf("Reserva feita com sucesso!\n");
+    
+    } else {
+        printf("N?o h? assentos dispon?veis para este voo!\n");
+    }
+}
+
+void consultarVoos(Voos *voo, int *numVoos){
+    for (int i = 0; i < numVoos; i++)
+    {
+        printf("\nOrigem: %s\n Destino: %s\n Data: %2d/%1d/%4d\n Hora: %2d:%1d\n Assentos Disponiveis: %d\n", voo[i].origem, voo[i].destino, voo[i].dia, voo[i].mes, voo[i].ano, voo[i].hora, voo[i].min, voo[i].assentosDispo);
+    }
+    
+}
+
+void excluirReserva(Passageiros *pessoas, Voos *voo, Reserva *reservas, int *numReservas){
+    int idPass, idVoo, vooParaExcluir;
+
+    printf("Digite o ID do passageiro para excluir a reserva (0 a %d)", MAX_PESSOAS - 1);
+    idPass = input_d("");
+
+    if (idPass < 0 || idPass >= MAX_PESSOAS)
+    {
+        printf("ID do Passageiro Invalido!\n");
+        return;
+    }
+
+    printf("Voos reservados pelo passageiro %s (ID %d):\n", pessoas[idPass].nome, idPass);
+    int voosReservados[MAX_PESSOAS];
+    int numVoosReservados = 0;
+
+    for (int i = 0; i < *numReservas; i++) {
+        if (reservas[i].idPassageiro == idPass) {
+            voosReservados[numVoosReservados] = i;
+            printf("Voo %d: Origem: %s, Destino: %s, Data: %02d/%02d/%04d, Hora: %02d:%02d\n", 
+                   numVoosReservados + 1,
+                   voo[reservas[i].idVoo].origem,
+                   voo[reservas[i].idVoo].destino,
+                   voo[reservas[i].idVoo].dia,
+                   voo[reservas[i].idVoo].mes,
+                   voo[reservas[i].idVoo].ano,
+                   voo[reservas[i].idVoo].hora,
+                   voo[reservas[i].idVoo].min);
+            numVoosReservados++;
+        }
+    }
+    if (numVoosReservados == 0)
+    {
+        printf("Nenhuma reserva encontrada para o passgeiro informado!\n");
+        return;
+    }
+    printf("Digite o número do voo que deseja excluir (1 a %d): ", numVoosReservados);
+    vooParaExcluir = input_d("");
+
+    if(vooParaExcluir < 1 || vooParaExcluir > numVoosReservados){
+        printf("Numero do voo invalido!\n");
+        return;
+    }
+    
+    int reservaIndex = voosReservados[vooParaExcluir - 1];
+    int idVooParaExcluir = reservas[reservaIndex]. idVoo;
+
+    voo[idVooParaExcluir].assentosDispo++;
+
+    for (int j = reservaIndex; j < *numReservas - 1; j++){
+        reservas[j] = reservas[j + 1];
+    }
+
+    (*numReservas)--;
+    printf("Reserva excluída com sucesso!\n");
+}
+
+void listarReservas(Reserva *reservas, int numReservas, Passageiros *pessoas, int numPassageiros, Voos *voo, int numVoos){
+    int idPass;
+    printf("Digite o ID do passageiro (0 a %d):", numPassageiros - 1);
+    idPass = input_d("");
+
+    if (idPass < 0 || idPass >= numPassageiros)
+    {
+        printf("ID do Passageiro Invalido!\n");
+        return;
+    }
+
+    printf("Reservas para o passageiro %s (Documento: %d):\n", pessoas[idPass].nome, pessoas[idPass].numDoc);
+    int reservasEncontradas = 0;
+    for (int i = 0; i < numReservas; i++)
+    {
+        if (reservas[i].idPassageiro == idPass)
+        {
+            int idVoo = reservas[i].idVoo;
+            printf("\nVoo %d:\n", i+1);
+            printf("Origem: %s\n", voo[idVoo].origem);
+            printf("Destino: %s\n", voo[idVoo].destino);
+            printf("Data: %02d/%02d/%04d\n", voo[idVoo].dia, voo[idVoo].mes, voo[idVoo].ano);
+            printf("Hora: %02d:%02d\n", voo[idVoo].hora, voo[idVoo].min);
+            printf("Assentos Disponiveis: %d\n", voo[idVoo].assentosDispo);
+            reservasEncontradas++;
+        }
+    }
+    if (reservasEncontradas == 0)
+    {
+        printf("Nenhuma reserva encontrada para o passageiro informado!\n");
+    }
+    
+    
+    
 }
 
 int main(int argc, char const *argv[])
 {
-    Passageiro pessoas[MAX_VETOR];
+    setlocale(LC_ALL, "Portuguese");
+    Passageiros pessoas[MAX_PESSOAS];
+    Voos voo[MAX_PESSOAS];
+    Reserva reservas[MAX_PESSOAS];
+    int count = 0;
+    int numPassageiros = 0;
+    int numVoos = 0;
+    int numReservas = 0;
     
-    fazerReserva(pessoas, "passageiros.txt");
-    
-    
-    //menu();
-    
+    numVoos = carregarVoos(voo, "voos.txt");
+    if (numVoos < 0) return 1;
+    numPassageiros = carregarPassageiros(pessoas, "passageiros.txt");
+    if (numPassageiros < 0) return 1;
+    //printf("NumPassageiros: %d %d\n", numPassageiros - 1, numVoos);
+    //fazerReservas(pessoas, &numPassageiros, voo, &numVoos, reservas, numReservas);
+    //printf("%d", numReservas);
+
+
+    int op = -1;
+    // caso o usuario digite qualquer numero diferente de 5 o menu continuara rodando em loop ate que o 5 seja digitado que ? a unica condi??o de parada
+    while (op != 6) {
+        do {
+            printf("***************************************************************************************\n");
+            printf(" 1 - Fazer Reserva do Voo                                                              \n");
+            printf(" 2 - Consultar Voos Disponiveis                                                        \n");
+            printf(" 3 - Listar Reservas                                                                   \n");
+            printf(" 4 - Excluir Reservas                                                                  \n");
+            printf(" 5 - Sair.                                                                             \n");
+            printf("***************************************************************************************\n");
+            op = input_d("Digite um numero entre: [1-5]: ");
+        } while (op < 1 || op > 5);
+
+        switch (op) {
+            case 1:
+                fazerReservas(pessoas, numPassageiros, voo, numVoos, reservas, &numReservas);
+                break;
+            case 2:
+                consultarVoos(voo, numVoos);
+                break;
+            case 3:
+                listarReservas(reservas, numReservas, pessoas, numPassageiros, voo, numVoos);
+                break;
+            case 4:
+                excluirReserva(pessoas, voo, reservas, &numReservas);
+                break;
+            case 5:
+                printf("Saindo...\n");
+                return;
+        }
+    }
     return 0;
 }
--- structs.h
+++ structs.h
@@ -1,21 +1,120 @@
+#include <stdio.h>
 #include <stdlib.h>
-#define MAX_LENGTH_ORIGIN 200
-#define MAX_LENGTH_NAME 300
-#define MAX_VETOR 100
+#include <string.h>
+#include <time.h>
+#include <locale.h>
+#include <limits.h>
+#include <inttypes.h>
+#include <errno.h>
+#include <float.h>
+#include "pss.h"
+
+#define MAX_PESSOAS 30
+#define MAX_NAME 100
+#define MAX_ORIGIN_DESTINY 200
 
 typedef struct{
-    char origem[MAX_LENGTH_ORIGIN];
-    char destino[MAX_LENGTH_ORIGIN];
-    float data;
-    float horario;
-    int assentos;
-}Voo;
+    char nome[MAX_NAME];
+    long int numDoc;
+} Passageiros;
 
+typedef struct {
+    char origem[MAX_ORIGIN_DESTINY];
+    char destino[MAX_ORIGIN_DESTINY];
+    int dia, mes, ano;
+    int hora;
+    int min;
+    int assentosDispo;
+}Voos;
 
-typedef struct{
-    char nome[MAX_LENGTH_NAME];
-    char segundo[MAX_LENGTH_NAME];
-    char sobrenome[MAX_LENGTH_NAME];
-    char ultimo[MAX_LENGTH_NAME];
-    long int numero_doc;
-}Passageiro;
+typedef struct {
+    int idPassageiro;
+    int idVoo;
+}Reserva;
+
+void limpa_buffer() {
+    int lixo;
+    while ((lixo = getchar()) != '\n' && lixo != '\r' && lixo != EOF);
+}
+
+char input_c(const char *mensagem) {
+    char x;
+    do {
+        printf("%s", mensagem);
+        x = fgetc(stdin); 
+        if (x == '\n') continue;
+        if (x != EOF) limpa_buffer();
+    } while (x == '\n' || x == EOF); 
+    return x;
+}
+
+void input_s(const char *mensagem, char *x, int tamanho) {	
+    do {
+        printf("%s", mensagem);
+        if (fgets(x, tamanho,stdin) != NULL) 
+            x[strcspn(x, "\n")] = '\0';
+    } while (strlen(x) == 0);
+    if ( strlen(x) == tamanho - 1 ) limpa_buffer();    
+}
+
+int input_d(const char *mensagem) {
+    char aux[1024];
+    char *endptr;
+    int x;
+    intmax_t temp;
+    do{
+        input_s(mensagem,aux, sizeof(aux));
+        temp = strtoimax(aux, &endptr, 10);
+        if ((temp == INTMAX_MIN || temp == INTMAX_MAX) && errno == ERANGE) temp = INT_MAX;
+        x = (int)temp;
+    }while(*endptr != '\0');
+    return x;
+}
+
+float input_f(const char *mensagem) {
+    char aux[1024];
+    char *endptr;
+    float x;
+    do{
+        input_s(mensagem, aux, sizeof(aux));
+        x = strtof(aux, &endptr);
+        if ((x < FLT_MIN || x > FLT_MAX)) x = FLT_MAX;
+    }while(*endptr != '\0');
+    return x;
+}
+
+long int  input_ld(const char *mensagem) {
+    char aux[1024];
+    char *endptr;
+    long int x;
+    do{
+        input_s(mensagem, aux, sizeof(aux));
+        x = strtol(aux, &endptr, 10);
+        if ((x < LONG_MIN || x > LONG_MAX)) x = LONG_MAX;
+    }while(*endptr != '\0');
+    return x;
+}
+
+double input_lf(const char *mensagem) {
+    char aux[1024];
+    char *endptr;
+    double x;
+    do{
+        input_s(mensagem, aux, sizeof(aux));
+        x = strtod(aux, &endptr); 
+        if ((x < DBL_MIN || x > DBL_MAX)) x = DBL_MAX;
+    }while(*endptr != '\0');
+	return x; 
+}
+
+long double input_Lf(const char *mensagem) {
+    char aux[5120];
+    char *endptr;
+    long double x;
+    do{
+        input_s(mensagem, aux, sizeof(aux));
+        x = strtold(aux, &endptr); 
+        if ((x < LDBL_MIN || x > LDBL_MAX)) x = LDBL_MAX;
+    }while(*endptr != '\0'); 
+    return x; 
+}
